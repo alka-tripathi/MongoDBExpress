7ece9aa4497beceebbae86229aa866b77ab811e5
Alka Tripathi <tripathialka382@gmail.com>
2025-03-27 11:27:54 +0530

    add button is formed

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let express=require("express");
 const app=express();
 const port=8080;
 
+//use to parse the data
+app.use(express.urlencoded({ extended: true }));
+
 // app.use(express.static("public"));
+
 //requiring views folder for ejs files
 const path =require("path");
 app.set("views",path.join(__dirname,"views"));
@@ -13,6 +17,7 @@ app.set("view engine","ejs");
 //requiring moongose
 
 const mongoose = require('mongoose');
+
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/Telegram');
   }
@@ -25,7 +30,6 @@ main().then((res)=>{
 });
 
 
-
 let Chat = require("./models/chats.js");
 app.listen(port,()=>{
     console.log(`${port} is listening`);
@@ -35,9 +39,32 @@ app.get("/",(req,res)=>{
 });
 
 //homepage
+
 app.get("/home",async(req,res)=>{
+
     const chats = await Chat.find();
     res.render("index.ejs",{chats});
 
+});
 
+
+//new button
+app.get("/home/new",(req,res)=>{
+    // res.send("in form apge");
+    res.render("new.ejs");
+})
+
+app.post("/home",(req,res)=>{
+    let {username,message}=req.body;
+    let newchats = new Chat({
+        name:username,
+        message:message,
+    });
+    newchats.save().then((res)=>{
+      console.log("data is saved");
+    })
+    .catch((err)=>{
+        console.log("error is:" +err);
+    })
+    res.redirect("/home");
 })
